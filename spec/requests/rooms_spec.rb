7a6acc81c1229083a4a01a0fa29ae6a915b91a99
cheesecomer <orz.pgr.m9@gmail.com>
2017-10-24 09:35:30 +0900

    参加者数をレスポンスに追加

--- spec/requests/rooms_spec.rb
+++ spec/requests/rooms_spec.rb
@@ -21,5 +21,12 @@ RSpec.describe 'Rooms', type: :request do
       it { expect(subject).to have_http_status :ok }
       it { expect(JSON.parse(subject.body).symbolize_keys).to eq rooms:[] }
     end
+    context 'when not empty' do
+      let(:user) { FactoryGirl.create(:user) }
+      let!(:rooms) { FactoryGirl.create_list(:room, 10) }
+      let(:optional_header) { { authorization: "Token #{user.access_token}" } }
+      it { expect(subject).to have_http_status :ok }
+      it { expect(JSON.parse(subject.body).deep_symbolize_keys).to eq rooms: rooms.map {|v| v.attributes.symbolize_keys.slice(:id, :name, :capacity).merge number_of_participants: 0 } }
+    end
   end
 end
