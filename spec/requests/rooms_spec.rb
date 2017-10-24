cdcd10cf1dbd7cbc67476a62f9447f959dc89d76
cheesecomer <orz.pgr.m9@gmail.com>
2017-10-24 09:22:28 +0900

    APIテストコードで認証ヘッダの付与

--- spec/requests/rooms_spec.rb
+++ spec/requests/rooms_spec.rb
@@ -7,12 +7,18 @@ RSpec.describe 'Rooms', type: :request do
       response
     end
     let(:request_header) do
-      { 'content-type': 'application/json', accept: 'application/json' }
+      { 'content-type': 'application/json', accept: 'application/json' }.merge optional_header
     end
     let(:error_response) { { message: I18n.t('devise.failure.unauthenticated') } }
     context 'when unauthorized' do
+      let(:optional_header) { {} }
       it { expect(subject).to have_http_status :unauthorized }
       it { expect(JSON.parse(subject.body).symbolize_keys).to eq error_response }
+    end
+    context 'when empty' do
+      let(:user) { FactoryGirl.create(:user) }
+      let(:optional_header) { { authorization: "Token #{user.access_token}" } }
+      it { expect(subject).to have_http_status :ok }
     end
   end
 end
