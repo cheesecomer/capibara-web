632607e7f54bc671eeb7cfa382088f6c282cf14f
cheesecomer <orz.pgr.m9@gmail.com>
2017-10-24 12:29:35 +0900

    Add codecov

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -18,6 +18,9 @@ require 'simplecov'
 
 SimpleCov.start 'rails'
 
+require 'codecov'
+SimpleCov.formatter = SimpleCov::Formatter::Codecov
+
 RSpec.configure do |config|
   # rspec-expectations config goes here. You can use an alternate
   # assertion/expectation library such as wrong or the stdlib/minitest
