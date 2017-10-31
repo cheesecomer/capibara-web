5c2e83523ce00fea843193155c838c62b04f3e5b
cheesecomer <orz.pgr.m9@gmail.com>
2017-10-31 10:39:26 +0900

    サインイン画面リクエストスペック

---
+++ spec/requests/session_spec.rb
@@ -0,0 +1,39 @@
+require 'rails_helper'
+
+RSpec.describe 'SessionsController', type: :request do
+  describe 'GET /session/new' do
+    subject do
+      get '/session/new'
+      response
+    end
+    context 'When not signin' do
+      it { expect(subject).to have_http_status :ok }
+    end
+    context 'When signin' do
+      let(:user) { FactoryGirl.create(:user) }
+      before { sign_in user }
+      it { expect(subject).to have_http_status :found }
+    end
+  end
+
+  describe 'POST /session' do
+    let(:user) { FactoryGirl.create(:user) }
+    subject do
+      post '/session', params: { user: request_body }
+      response
+    end
+    context 'When valie' do
+      let(:request_body) { { email: user.email, password: 'password' } }
+      it { expect(subject).to have_http_status :found }
+    end
+    context 'When invalie' do
+      let(:request_body) { { email: user.email, password: 'xxxxxxxx' } }
+      it { expect(subject).to have_http_status :ok }
+    end
+    context 'When signin' do
+      let(:request_body) { { email: user.email, password: 'password' } }
+      before { sign_in user }
+      it { expect(subject).to have_http_status :found }
+    end
+  end
+end
--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -21,11 +21,11 @@ RSpec.describe 'Users', type: :request do
       post '/users', params: { user: request_body }
       response
     end
-    context 'When valie' do
+    context 'When invalie' do
       let(:request_body) { {} }
       it { expect(subject).to have_http_status :ok }
     end
-    context 'When invalie' do
+    context 'When valie' do
       let(:request_body) { { nickname: FFaker::Name.name, email: FFaker::Internet.email, password: 'password', password_confirmation: 'password' } }
       it { expect(subject).to have_http_status :found }
       it { expect { subject }.to change { User.all.count }.by(1) }
