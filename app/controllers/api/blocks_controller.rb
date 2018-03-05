c10c0551405e32bbbdc6079dc6a6b4263d9a9f78
cheesecomer <orz.pgr.m9@gmail.com>
2018-03-05 18:43:28 +0900

    ブロック登録APIの実装

--- app/controllers/api/blocks_controller.rb
+++ app/controllers/api/blocks_controller.rb
@@ -2,4 +2,13 @@ class Api::BlocksController < Api::ApplicationController
   def index
     @blocks = Block.where(owner: current_user)
   end
+  def create
+    Block.create! create_params.merge(owner: current_user)
+    @blocks = Block.where(owner: current_user)
+    render :index
+  end
+
+  def create_params
+    params.require(:block).permit(:target_id)
+  end
 end
--- spec/requests/api/blocks_spec.rb
+++ spec/requests/api/blocks_spec.rb
@@ -33,4 +33,33 @@ RSpec.describe 'Blocks', type: :request do
       it { expect(JSON.parse(subject.body, symbolize_names: true)).to eq blocks: blocks.map {|v| { id: v[:id], target: { id: v.target.id, nickname: v.target.nickname } } } }
     end
   end
+
+  describe 'POST /api/blocks' do
+    subject do
+      post '/api/blocks', params: request_json, headers: request_header
+      response
+    end
+    let(:request_header) do
+      { 'content-type': 'application/json', accept: 'application/json' }.merge optional_header
+    end
+    let(:error_response) { { message: I18n.t('devise.failure.unauthenticated') } }
+    let(:target) { FactoryGirl.create(:user) }
+    context 'when unauthorized' do
+      let(:optional_header) { {} }
+      let(:request_json) { { target_id: target.id }.to_json }
+      it { expect(subject).to have_http_status :unauthorized }
+      it { expect(JSON.parse(subject.body, symbolize_names: true)).to eq error_response }
+    end
+    context 'when authorized' do
+      let(:user) { FactoryGirl.create(:user) }
+      let(:other_user) { FactoryGirl.create(:user) }
+      let(:request_json) { { target_id: target.id }.to_json }
+      let!(:blocks) { FactoryGirl.create_list(:block, 10, owner: user) }
+      let!(:other_blocks) { FactoryGirl.create_list(:block, 10, owner: other_user) }
+      let(:optional_header) { { authorization: "Token #{user.access_token}" } }
+      it { expect(subject).to have_http_status :ok }
+      it { expect { subject }.to change { Block.all.count }.by(1) }
+      it { expect(JSON.parse(subject.body, symbolize_names: true)).to eq blocks: (blocks + [Block.last]).map {|v| { id: v[:id], target: { id: v.target.id, nickname: v.target.nickname } } } }
+    end
+  end
 end
