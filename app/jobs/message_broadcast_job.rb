b368b3e57cec4dd0021209112d5754dcc5f6b2d1
cheesecomer <orz.pgr.m9@gmail.com>
2018-03-14 13:51:40 +0900

    ブロック中ユーザーからのメッセージがブロードキャストされないように修正

--- app/jobs/message_broadcast_job.rb
+++ app/jobs/message_broadcast_job.rb
@@ -3,6 +3,7 @@ class MessageBroadcastJob < ApplicationJob
 
   def perform(message)
     ChatChannel.connected_users(message.room) \
+      .select{ |v| !(Block.exists?(owner: v, target: message.sender) || Block.exists?(owner: message.sender, target: v)) } \
       .each do |user|
         ChatChannel.broadcast_to [message.room, user], message.to_broadcast_hash
       end
