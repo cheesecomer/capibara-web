a0e73f265d0da6779aaede7df3f8274ca2a18000
cheesecomer <orz.pgr.m9@gmail.com>
2018-03-18 22:59:34 +0900

    退会済みユーザーが送信したメッセージに対する顧慮を追加

--- spec/requests/api/rooms_spec.rb
+++ spec/requests/api/rooms_spec.rb
@@ -76,5 +76,14 @@ RSpec.describe 'Rooms', type: :request do
       it { expect(JSON.parse(subject.body, symbolize_names: true)).to eq id: room.id, name: room.name, capacity: room.capacity, number_of_participants: 0, participants: [], messages: [
         { sender: { id: message.sender.id, nickname: message.sender.nickname, icon_url: message.sender.icon_url }, id: message.id, content: message.content, at: message.created_at.iso8601(3) }] }
     end
+    context 'when deleted' do
+      let(:user) { FactoryGirl.create(:user) }
+      let(:other_user) { FactoryGirl.create(:user).destroy }
+      let!(:message) { FactoryGirl.create(:message, sender: other_user, room: room)}
+      let(:optional_header) { { authorization: "Token #{user.access_token}" } }
+      it { expect(subject).to have_http_status :ok }
+      it { expect(JSON.parse(subject.body, symbolize_names: true)).to eq id: room.id, name: room.name, capacity: room.capacity, number_of_participants: 0, participants: [], messages: [
+        { sender: nil, id: message.id, content: message.content, at: message.created_at.iso8601(3) }] }
+    end
   end
 end
