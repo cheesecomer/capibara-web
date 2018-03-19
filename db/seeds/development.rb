7b9ece44e59844d0944b64ce1185a3ff6efff5f2
cheesecomer <orz.pgr.m9@gmail.com>
2018-03-19 10:49:29 +0900

    お知らせと削除済みユーザーの seed を追加

--- db/seeds/development.rb
+++ db/seeds/development.rb
@@ -1,6 +1,8 @@
 puts 'Create users'
 odd_user = FactoryGirl.create :user, email: 'user1@email.com', password: 'password'
 evn_user = FactoryGirl.create :user, email: 'user2@email.com', password: 'password'
+deleted_user = FactoryGirl.create :user, email: 'user3@email.com', password: 'password'
+deleted_user.destroy
 
 puts 'Create rooms'
 FactoryGirl.create_list :room, 10
@@ -11,4 +13,8 @@ Room.all.each do |room|
     FactoryGirl.create :message, room: room, sender: odd_user
     FactoryGirl.create :message, room: room, sender: evn_user
   end
+  FactoryGirl.create :message, room: room, sender: deleted_user
 end
+
+puts 'Create information'
+FactoryGirl.create_list :information, 10
