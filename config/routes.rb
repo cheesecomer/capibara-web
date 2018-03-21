417490899f8d8873d4904b219074b6811ec006d5
cheesecomer <orz.pgr.m9@gmail.com>
2018-03-21 16:20:59 +0900

    プライバシーポリシーの追加

--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,8 @@ Rails.application.routes.draw do
 
   resources :rooms
   resources :users, only: [:show]
+  resource :privacy_policy, only: [:show]
+  resources :inquiries, only: [:new, :create]
 
   namespace :api, defaults: { format: :json } do
     resource :session, only: [:create, :destroy], controller: :sessions
