f36e3d66534478874059ad524386d3a4d4242cf3
cheesecomer <orz.pgr.m9@gmail.com>
2018-03-24 15:40:46 +0900

    ファイル名ランダムに変更

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -13,6 +13,16 @@ class ImageUploader < CarrierWave::Uploader::Base
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
 
+  def filename
+    "#{secure_token}.#{file.extension}" if original_filename.present?
+  end
+
+  protected
+  def secure_token
+    var = :"@#{mounted_as}_secure_token"
+    model.instance_variable_get(var) or model.instance_variable_set(var, SecureRandom.uuid)
+  end
+
   # Provide a default URL as a default if there hasn't been a file uploaded:
   # def default_url(*args)
   #   # For Rails 3.1+ asset pipeline compatibility:
