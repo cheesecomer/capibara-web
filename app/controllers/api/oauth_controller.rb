4ec7b1366eefb6fc78f5bad2633de1fd82daff4b
cheesecomer <orz.pgr.m9@gmail.com>
2018-03-25 20:09:19 +0900

    OAuth 時に HTTP リクエストヘッダの検証をスキップ

--- app/controllers/api/oauth_controller.rb
+++ app/controllers/api/oauth_controller.rb
@@ -1,5 +1,6 @@
 class Api::OauthController < Api::ApplicationController
   skip_before_action :authenticate_user_from_token!
+  skip_before_action :verify_http_header!
   def callback
     user = User.find_or_create_from_oauth(request.env['omniauth.auth'])
     redirect_to "com.cheesecomer.capibara://oauth?access_token=#{user.access_token}&id=#{user.id}"
