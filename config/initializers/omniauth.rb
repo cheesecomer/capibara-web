fb62cc2082658b8939225b0121f6ca7258c6f6d9
cheesecomer <orz.pgr.m9@gmail.com>
2018-03-26 12:13:13 +0900

    omniauth-google-oauth2 の設定を追加

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -4,4 +4,5 @@ Rails.application.config.middleware.use OmniAuth::Builder do
   end
   provider :twitter, ENV["TWITTER_CONSUMER_KEY"], ENV["TWITTER_CONSUMER_SECRET_KEY"]
   provider :line, ENV["LINE_CHANNEL_ID"], ENV["LINE_CHANNEL_ID_SECRET"]
+  provider :google_oauth2, ENV['GOOGLE_CLIENT_ID'], ENV['GOOGLE_CLIENT_SECRET'], name: :google, scope: %w(email profile), image_size: 500
 end
