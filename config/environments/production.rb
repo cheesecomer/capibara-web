33533443e0a10edb4324b629d6e009032b289595
cheesecomer <orz.pgr.m9@gmail.com>
2018-03-31 12:46:39 +0900

    fog 設定を移植

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -92,4 +92,13 @@ end
 
 CarrierWave.configure do |config|
   config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/capibara'
+  config.fog_credentials = {
+    provider: 'AWS',
+    aws_access_key_id: ENV['AWS_S3_ACCESS_KEY'],
+    aws_secret_access_key: ENV['AWS_S3_SECRET_KEY'],
+    region: 'ap-northeast-1'
+  }
+
+  config.fog_directory  = 'capibara'
+  config.cache_storage = :fog
 end
