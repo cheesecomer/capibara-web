3adc125153eecb2c5b122ab19c870770b05f4a21
cheesecomer <orz.pgr.m9@gmail.com>
2018-04-01 23:02:01 +0900

    未使用のコントローラーを削除

--- app/controllers/rooms_controller.rb
+++
@@ -1,9 +0,0 @@
-class RoomsController < ApplicationController
-  def show
-    @room = Room.find(params[:id])
-  end
-
-  def index
-    @rooms = Room.all
-  end
-end
--- app/controllers/user_registrations_controller.rb
+++
@@ -1,9 +0,0 @@
-class UserRegistrationsController < Devise::RegistrationsController
-  def new
-    super
-  end
-
-  def create
-    super
-  end
-end
