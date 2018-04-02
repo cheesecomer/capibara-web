80c7d7e5b6f2e23507cab352ea1c5b4258b52656
cheesecomer <orz.pgr.m9@gmail.com>
2018-04-02 15:00:26 +0900

    お知らせ一覧の実装

--- app/controllers/informations_controller.rb
+++ app/controllers/informations_controller.rb
@@ -1,5 +1,10 @@
 class InformationsController < ApplicationController
   skip_before_action :authenticate_admin!, only: %i[show]
+  def index
+    @informations = Information.order(published_at: :desc)
+    render layout: 'admin'
+  end
+
   def show
     @information = Information.find(params[:id])
     raise ActiveRecord::RecordNotFound unless @information.published?
--- spec/requests/informations_spec.rb
+++ spec/requests/informations_spec.rb
@@ -1,6 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe InformationsController, type: :request do
+  describe 'GET #index' do
+    subject do
+      get informations_url
+      response
+    end
+    context 'When not signin' do
+      it { is_expected.to have_http_status :found }
+    end
+    context 'When signin' do
+      let(:admin) { FactoryBot.create(:admin) }
+      before { sign_in admin }
+      it { is_expected.to have_http_status :ok }
+      it 'when valid should execute order by published_at desc' do
+        expect(Information).to receive(:order).with(published_at: :desc).and_return(Information.all)
+        subject
+      end
+    end
+  end
+
   describe 'GET #show' do
     subject do
       get information_url information
