665919a810264446a2bf81f5e75b9d54a7131de9
cheesecomer <orz.pgr.m9@gmail.com>
2018-04-02 15:24:05 +0900

    seed に管理者と通報を追加

--- db/seeds/development.rb
+++ db/seeds/development.rb
@@ -1,4 +1,5 @@
 puts 'Create users'
+FactoryBot.create :admin, email: 'admin@email.com', password: 'password'
 odd_user = FactoryBot.create :user, email: 'user1@email.com', password: 'password'
 evn_user = FactoryBot.create :user, email: 'user2@email.com', password: 'password'
 deleted_user = FactoryBot.create :user, email: 'user3@email.com', password: 'password'
@@ -18,3 +19,7 @@ end
 
 puts 'Create information'
 FactoryBot.create_list :information, 10
+
+puts 'Create reports'
+FactoryBot.create_list :report, 5, sender: odd_user
+FactoryBot.create_list :report, 5, sender: evn_user
