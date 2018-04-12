2fb89bf2215ea567a84b8e223abd06c39ca7f533
cheesecomer <orz.pgr.m9@gmail.com>
2018-04-12 09:41:05 +0900

    filter_parameters 設定の移動

--- config/application.rb
+++ config/application.rb
@@ -24,7 +24,5 @@ module Capibara
       'iOS': Gem::Version.new('1.0'),
       'Android': Gem::Version.new('1.0')
     }.with_indifferent_access
-
-    config.filter_parameters += ['password', 'icon']
   end
 end
