322478761c22d9283f857c30d17445e68e80acb4
cheesecomer <orz.pgr.m9@gmail.com>
2018-04-29 22:49:19 +0900

    アソシエーションの削除

--- spec/models/room_spec.rb
+++ spec/models/room_spec.rb
@@ -14,10 +14,6 @@
 require 'rails_helper'
 
 RSpec.describe Room, type: :model do
-  describe "Associations" do
-    it { is_expected.to have_many(:participants) }
-    it { is_expected.to have_many(:users) }
-  end
   describe '#name' do
     it { is_expected.to validate_presence_of(:name) }
   end
