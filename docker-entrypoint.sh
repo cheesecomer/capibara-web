cbcbb603727265685eefe56b7eb773add35d887a
cheesecomer <orz.pgr.m9@gmail.com>
2018-05-04 01:12:41 +0900

    bundle exec を無効化

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,8 +3,8 @@
 export WORKDIR=$(pwd)
 cd /var/capibara
 
-bundle exec rake assets:precompile
-bundle exec rails db:migrate
+# bundle exec rake assets:precompile
+# bundle exec rails db:migrate
 
 cd $WORKDIR
 exec "$@"
