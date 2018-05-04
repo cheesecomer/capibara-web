8a4a8a550a02acaa06deb7f53f2dcacb3e9359ea
cheesecomer <orz.pgr.m9@gmail.com>
2018-05-04 18:07:14 +0900

    access_key_id と secret_access_key の設定

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,8 +3,8 @@
 export WORKDIR=$(pwd)
 cd /var/capibara
 
-bundle exec rake assets:precompile --trace &>> ./log/production.log
-bundle exec rails db:migrate
+# rake assets:precompile --trace
+# rails db:migrate
 
 cd $WORKDIR
 exec "$@"
